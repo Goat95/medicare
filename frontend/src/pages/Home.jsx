c78bcee80cf0eba9d2ac7d0cbaa7b6fef7482ada
Goat95 <hjh9703@naver.com>
2023-07-07 02:09:30 +0900

    feat: implement service section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
 
 import About from "../components/About/About";
+import ServiceList from "../components/Services/ServiceList";
 
 const Home = () => {
   return (
@@ -153,7 +154,23 @@ const Home = () => {
         </div>
       </section>
 
+      {/* about section */}
       <About />
+
+      {/* services section */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our medical services</h2>
+            <p className="text__para text-center">
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+
+          <ServiceList />
+        </div>
+      </section>
     </>
   );
 };
